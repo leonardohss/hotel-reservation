ba62a3c162467dc6204a732cfb877a50dc83e851
Leonardo (NoteMauricy) <leorick97@gmail.com>
2019-07-06 10:08:46 -0300

    testing

--- HotelReservation/Program.cs
+++ HotelReservation/Program.cs
@@ -13,6 +13,8 @@ namespace HotelReservation
             DateTime checkIn = DateTime.Parse(Console.ReadLine());
             Console.Write("Check-out Date (dd/mm/yyyy): ");
             DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
         }
     }
 }
