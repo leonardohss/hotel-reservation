db82bf8979ebe7e137e58e16ef55ab5d06b6b11b
Leonardo (NoteMauricy) <leorick97@gmail.com>
2019-07-06 10:04:53 -0300

    Create the project

---
+++ HotelReservation/Entities/Reservation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Room: "
+                + RoomNumber
+                + ", Check-in: "
+                + Checkin.ToString("dd/MM/yyyy")
+                + ", Check-out: "
+                + Checkout.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ HotelReservation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HotelReservation.Entities;
+
+namespace HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in Date (dd/mm/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out Date (dd/mm/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+        }
+    }
+}
